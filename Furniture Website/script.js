fd795795a158c44cb976dd81112c20ee1243351d
GamesLegend <72524734+GamesLegend@users.noreply.github.com>
2023-01-19 14:35:30 +0400

    Add files via upload

---
+++ Furniture Website/script.js
@@ -0,0 +1,43 @@
+console.log("hello world");
+
+let ClassName = 5;
+let ClassName2 = 545;
+
+console.log(ClassName + ClassName2);
+console.log(Math.floor(Math.random() * 100));
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("navbar-list-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
